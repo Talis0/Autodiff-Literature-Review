93efe54a789afae04a1c2d7a74d2740e0136796a
Tapash Alister <87761309+Talis0@users.noreply.github.com>
2022-10-28 13:15:55 +1000

    Update and rename Sensitivity Analysis - 1v1o to Figure 4.py

--- Python/Figure 4.py
+++ Python/Figure 4.py
@@ -3,19 +3,20 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plot
 import math
 
-A = [1.]
-h = 0.1
-MaxT = 5
-steps = int(MaxT/h)
-u_0 = jnp.array([1])
+A = [1.] #Constant Value
+h = 0.1 #Step Size
+MaxT = 5 #Max Time
+steps = int(MaxT/h) #Number of Steps
+u_0 = jnp.array([1]) #Initial COndition
 
-
+#Definition of ODE
 def func(X,A):
         x = X[0]
         a = A[0]
         output = [-a*x]
         return jnp.array(output)
 
+#Implimentation of RK4
 def RK4(f,h,u_0,steps):
     u = jnp.zeros([len(u_0),steps])
     u = u.at[:,0].set(u_0)
@@ -30,7 +31,7 @@ def RK4(f,h,u_0,steps):
         u = u.at[:,i+1].set(k)
     return u[0]
 
-
+#f'(a)
 def ode(a):
     def f(X):
         return func(X,a)
@@ -40,23 +41,29 @@ def ode(a):
     return u
 
 
-sol = ode(A)
-der = jax.jacfwd(ode)(A)[0]
+sol = ode(A) #Solution
+der = jax.jacfwd(ode)(A)[0] #Derivative
 x = [x*h for x in range(steps)]
 
-plot.subplot(211)
+#Plotting
+plot.subplot(121)
 plot.title("Solution")
 plot.ylabel("f(a)")
 plot.plot(x,sol)
 
 
-
-plot.subplot(212)
+plot.subplot(122)
 plot.title("Derivative")
 plot.ylabel("f ' (a)")
 plot.xlabel("t")
-plot.plot(x,jac)
+plot.plot(x,der)
 
-plot.subplots_adjust(top=1.5)
+plot.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.4,
+                    hspace=0.4)
 
+plot.figure(figsize=(10,10)) 
 plot.show()
